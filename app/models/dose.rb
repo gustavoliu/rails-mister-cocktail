c4fd43c281140ccdf7f6a0b675c7f1681db8cf55
Gustavo Liu <gustavoliu@gmail.com>
2017-11-09 15:58:53 -0200

    Models set alright

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :cocktail, :ingredient, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
+
 end
